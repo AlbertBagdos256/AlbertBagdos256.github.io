dcfd11815297ac252d4733a3fb6c28edc8a53b55
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2019-05-05 18:51:04 +0500

    Add files via upload

---
+++ js/other.js
@@ -0,0 +1,309 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 355,
+        "density": {
+          "enable": true,
+          "value_area": 789.1476416322727
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.48927153781200905,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 0.2,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 2,
+          "size_min": 0,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.2,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 83.91608391608392,
+          "size": 1,
+          "duration": 3,
+          "opacity": 1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  })
+  $(document).ready(function () {
+    $('.particles-js').each(function (i, obj) {
+      let name = 'particle-' + i;
+      $(this).attr('id', name);
+      particlesJS(name, {
+        "particles": {
+          "number": {
+            "value": 104,
+            "density": {
+              "enable": true,
+              "value_area": 868.0624057955
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "grab"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 155.84415584415586,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      });
+    });
+    
+    var colors = new Array(
+      [49, 23, 23],
+      [35, 17, 41],
+      [6, 3, 7],
+      [7, 17, 107, 0],
+      [236, 129, 6],
+      [48, 39, 39]);
+    
+    var step = 0;
+    //color table indices for: 
+    // current color left
+    // next color left
+    // current color right
+    // next color right
+    var colorIndices = [0,1,2,3];
+    
+    //transition speed
+    var gradientSpeed = 0.002;
+    
+    function updateGradient()
+    {
+      
+      if ( $===undefined ) return;
+      
+    var c0_0 = colors[colorIndices[0]];
+    var c0_1 = colors[colorIndices[1]];
+    var c1_0 = colors[colorIndices[2]];
+    var c1_1 = colors[colorIndices[3]];
+    
+    var istep = 1 - step;
+    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+    var color1 = "rgb("+r1+","+g1+","+b1+")";
+    
+    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+    var color2 = "rgb("+r2+","+g2+","+b2+")";
+    
+     $('#gradient').css({
+       background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+        background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+      
+      step += gradientSpeed;
+      if ( step >= 1 )
+      {
+        step %= 1;
+        colorIndices[0] = colorIndices[1];
+        colorIndices[2] = colorIndices[3];
+        
+        //pick two new target color indices
+        //do not pick the same as the current one
+        colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+        colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+        
+      }
+    }
+    
+    setInterval(updateGradient,10);
+    
+    
+    
+    
+    
+    
+    
+     var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    console.log(traX);
+    $(".title").css({"background-position": traX + "%" + traY + "%"});
+  }); 
+  
+  
+  
+  });
---
+++ js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+  var $burger = $('.burger');
+  var $menu = $('.header-menu');
+  var $close = $('.close');
+  $links = $('.final');
+  $(window).scroll(function () { 
+      var scroll = $(this).scrollTop();
+      if (scroll > 800) {
+          $burger.fadeIn();
+      }
+  
+      else {
+          $burger.fadeOut();
+      }
+      console.log(scroll);
+   })
+  $burger.click(function () {
+      $menu.addClass('menu-active')
+     if ($menu.hasClass('menu-active')) {
+      $close.show()
+      $burger.hide()
+  }
+  else {
+    $close.hide()
+    $burger.show()
+  }  
+  })
+ $close.click(function (e) {
+     e.preventDefault()
+     $menu.removeClass('menu-active')
+     if ($menu.hasClass('menu-active')) {
+        $close.show()
+        
+    }
+    else {
+      $close.hide()
+      $burger.show()
+    } 
+   })
+   
+  
+   $links.click(function (e) {
+       e.preventDefault();
+       $links.removeClass('active-link');
+       var attr = $(this).addClass('active-link').attr('href'); //#home
+       var target = $(attr).offset().top //get from home find coordinations of sections
+       
+       $('body, html').animate({
+          scrollTop: target 
+       });
+   })
+   
+   $(window).scroll(function () {
+       var scroll = $(this).scrollTop()
+       $links.each(function () {
+           var id = $(this).attr('href');
+           var selector = $(id).offset().top
+           if (scroll >= selector) {
+            $links.removeClass('active-link');
+               $(this).addClass('active-link');
+           }
+         })
+       
+   })
+});
